281aa2db2391b4c7e6983b0334ba65ed0eb84b34
danielleniz <danielmleniz@gmail.com>
2023-05-29 17:47:17 +0200

    merge

---
+++ src/api.js
@@ -0,0 +1,6 @@
+export const geoApiOptions = {
+    method: 'GET',
+}
+
+export const REACT_APP_API_KEY = "7cfbd1a3721cd8917da71986cb08bc8b";
+export const REACT_APP_API_URL = "https://api.openweathermap.org/data/2.5"
